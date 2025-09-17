262c3eda4899ffb7063ab074ba8134c0b7d15729
firmanhidayatullah-afk <hidayatullahfirman246@gmail.com>
2025-09-17 21:24:34 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-// Script interaktif sederhana
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("contactForm");
-  if (form) {
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      alert("Pesan berhasil dikirim! (simulasi, belum ada backend)");
-      form.reset();
-    });
-  }
+// Ambil semua card
+const serviceCards = document.querySelectorAll('.service-card');
+
+window.addEventListener('scroll', () => {
+  const triggerBottom = window.innerHeight * 0.9;
+
+  serviceCards.forEach(card => {
+    const cardTop = card.getBoundingClientRect().top;
+
+    if (cardTop < triggerBottom) {
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+      card.style.transition = "all 0.8s ease";
+    }
+  });
 });
